4ef597236987f90af0c40385dcaffd512748b1ab
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2023-12-28 11:30:20 +0100

    Database set up and connected, schema created.

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,17 +1,65 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
+import { connectToDB } from "../mongoose";
 import { scrapedAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
+        connectToDB();
+
         const scrapedProduct = await scrapedAmazonProduct(productUrl);
 
         if(!scrapedProduct) return;
 
+        let product = scrapedProduct;
+
+        const existingProduct = await Product.findOne({ url: scrapedProduct.url});
+
+        if(existingProduct) {
+            const updatePriceHistory: any = [
+                ...existingProduct.priceHistory,
+                { price: scrapedProduct.currentPrice}
+            ]
+
+            product = {
+                ...scrapedProduct,
+                priceHistory: updatePriceHistory,
+                lowestPrice: getLowestPrice(updatePriceHistory),
+                highestPrice: getHighestPrice(updatePriceHistory),         
+                averagePrice: getAveragePrice(updatePriceHistory),  
+            }
+
+        }
+
+        const newProduct = await Product.findOneAndUpdate({
+            url: scrapedProduct.url },
+            product,
+            { upsert: true, new: true }
+        );
+
+        revalidatePath(`/products/${newProduct._id}`)
+
         
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error}`)
+    }
+}
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({_id: productId});
+
+        if(!product) return null;
+
+        return product;
+    } catch (error) {
+        
     }
 }
